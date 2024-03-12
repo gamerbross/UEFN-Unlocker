0407371fa11c28383cd691e5728ca47ccc794242
gamerbross <gamerbross@>
2024-03-12 19:01:14 +0100

    update version (i always forget)

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -29,7 +29,7 @@ inline void writeMemory(const uintptr_t address, const std::vector<BYTE> toWrite
 
 void Main(const HMODULE hModule) {
     AllocConsole();
-    SetConsoleTitleA("UEFN Unlocker by gamerbross v1.2");
+    SetConsoleTitleA("UEFN Unlocker by gamerbross v1.3");
     FILE* pFile;
     freopen_s(&pFile, ("CONOUT$"), "w", stdout);
 
